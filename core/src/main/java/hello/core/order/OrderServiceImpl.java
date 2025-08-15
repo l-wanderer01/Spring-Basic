5a72c5b0fc4287c51a72782383417cb68348f986
l-wanderer01 <jhjjang0423@naver.com>
2025-08-15 15:56:40 +0900

    18. 새로운 할인 정책 적용과 문제점

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,7 +11,10 @@ public class OrderServiceImpl implements OrderService {
 
     // MemberRepository에서 멤버 찾기
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 정률 할인 정책 적용
+    private DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경. but, NullPointerException(NPE) 발생
+    // 해결 방법 -> 누군가가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주어야 한다!
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
