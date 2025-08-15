8117ca573b449989a4e242ff099d149f8291ad56
l-wanderer01 <jhjjang0423@naver.com>
2025-08-15 17:16:02 +0900

    [feat] 21. 새로운 구조와 할인 정책 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,7 +10,7 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성함
+// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성함 (애플리케이션을 구성하는 영역)
 public class AppConfig  {
 
     // AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다.
@@ -26,6 +27,7 @@ public class AppConfig  {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
