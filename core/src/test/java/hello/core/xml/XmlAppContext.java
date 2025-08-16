948366eaf5b8cac46d2c8cd586afd89d39d7fefb
l-wanderer01 <jhjjang0423@naver.com>
2025-08-17 01:36:50 +0900

    [feat] 33. 스프링 빈 설정 메타 정보 - BeanDefinition

--- core/src/test/java/hello/core/xml/XmlAppContext.java
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -11,7 +11,9 @@ public class XmlAppContext {
 
     @Test
     void xmlAppContext() {
+
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
