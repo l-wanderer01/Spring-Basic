3e4d0c06a8a6275211f7b075cfbcbbffce56437f
l-wanderer01 <jhjjang0423@naver.com>
2025-08-20 12:29:20 +0900

    37. 싱글톤 방식의 문제점

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);// 빈 조회
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);// 빈 조회
+
+        // ThreadA : A 사용자가 10000원 주문
+        statefulService1.order("userA", 10000);
+        // ThreadB : B 사용자가 20000원 주문
+        statefulService1.order("userA", 20000);
+
+        // ThreadA : 사용자 A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price); // 만원이 조회될 것을 기대하지만 2만원이 조회됨
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
