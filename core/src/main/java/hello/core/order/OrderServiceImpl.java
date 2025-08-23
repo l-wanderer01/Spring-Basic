1dc893e17b5cbece1c82a177fc8f90e2fba4ae62
l-wanderer01 <jhjjang0423@naver.com>
2025-08-23 17:40:47 +0900

    47. 롬복과 최신 트랜드

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 롬복
 public class OrderServiceImpl implements OrderService {
 
     // MemberRepository에서 멤버 찾기
@@ -31,10 +33,10 @@ public class OrderServiceImpl implements OrderService {
 //    }
 
     // @Autowired // 생성자에 Autowired를 작성하면 자동 의존관계 주입이 가능 (생성자가 하나하면, 생략해도 자동 주입 가능)
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     // 일반 메서드 주입
 //    @Autowired
